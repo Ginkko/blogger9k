9487b9b45fad4caafa1ae8650a501e58cc9e448e
Guest User <student@epicodus.com>
2015-06-15 14:36:00 -0700

    pull in all files from death-star

--- app/models/post.js
+++ app/models/post.js
@@ -3,7 +3,5 @@ import DS from 'ember-data';
 var attr = DS.attr;
 
 export default DS.Model.extend({
-  author: DS.belongsTo('author'),
-  title: attr('string'),
-  body: attr('string')
+  tweet: attr('string')
 });
--- server/mocks/posts.js
+++ server/mocks/posts.js
@@ -1,57 +1,15 @@
-if (!Array.prototype.find) {
-  Array.prototype.find = function(predicate) {
-    if (this == null) {
-      throw new TypeError('Array.prototype.find called on null or undefined');
-    }
-    if (typeof predicate !== 'function') {
-      throw new TypeError('predicate must be a function');
-    }
-    var list = Object(this);
-    var length = list.length >>> 0;
-    var thisArg = arguments[1];
-    var value;
-
-    for (var i = 0; i < length; i++) {
-      value = list[i];
-      if (predicate.call(thisArg, value, i, list)) {
-        return value;
-      }
-    }
-    return undefined;
-  };
-}
-
-var posts = [
-  {
-    id: 1,
-    title: 'Batman',
-    author: 1,
-    body: 'nanananananananananananananananan Batman'
-  },
-  {
-    id: 2,
-    title: 'Aquaman',
-    author: 1,
-    body: 'magikarp splash!'
-  }
-];
-
-var authors = [
-  {
-    id: 1,
-    name: "Stan Lee",
-    posts: [1,2]
-  }
-];
-
 module.exports = function(app) {
   var express = require('express');
   var postsRouter = express.Router();
 
   postsRouter.get('/', function(req, res) {
     res.send({
-      'posts': posts,
-      "authors": authors
+      'posts': [
+        {
+          id: 1,
+          tweet: 'ol Ben'
+        }
+      ]
     });
   });
 
@@ -61,10 +19,9 @@ module.exports = function(app) {
 
   postsRouter.get('/:id', function(req, res) {
     res.send({
-      'post': posts.find(function(post) {
-        return post.id == req.params.id
-      }),
-      'authors': authors
+      'posts': {
+        id: req.params.id
+      }
     });
   });
 
